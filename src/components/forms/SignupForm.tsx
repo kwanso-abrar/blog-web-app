ce521be46ae1aff62c9eeaaa1fbfe8661e067aff
Abrar Azam <abrar.azam@kwanso.com>
2023-03-08 14:58:15 +0500

    design signup page and signup form

--- src/components/forms/SignupForm.tsx
+++ src/components/forms/SignupForm.tsx
@@ -1,5 +1,83 @@
+import { styled } from '@mui/material/styles';
+import { useFormik } from 'formik';
+import { Box, Button, Grid, Link, TextField } from '@mui/material';
+import yupSchema from 'utils/yupSchema';
+
+const Wrapper = styled(Box)(() => ({}));
+
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: yupSchema.signUp,
+    onSubmit: (values) => {
+      console.log('Signup form submitted: ', values);
+    },
+  });
+
+  return (
+    <Wrapper>
+      <Box>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            id="name"
+            name="name"
+            label="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+            sx={{ marginTop: '10px' }}
+          />
+
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            sx={{ marginTop: '10px' }}
+          />
+
+          <TextField
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            sx={{ marginTop: '10px' }}
+          />
+
+          <Button color="primary" variant="contained" fullWidth type="submit" sx={{ marginTop: '10px' }}>
+            Submit
+          </Button>
+        </form>
+        <Grid container sx={{ marginTop: '5px' }}>
+          <Grid item xs>
+            {/* <Link href="#" variant="body2">
+              Forgot password?
+            </Link> */}
+          </Grid>
+          <Grid item>
+            <Link href="/signin" variant="body2">
+              {'Already have an account? Sign In'}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Wrapper>
+  );
 };
 
 export default SignupForm;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,40 @@
+import { styled } from '@mui/material/styles';
+import { SignupForm } from 'components/forms';
+import { Box, Typography } from '@mui/material';
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  paddingTop: '88px',
+  minHeight: '100vh',
+  backgroundColor: theme.palette.secondary.contrastText,
+
+  // [theme.breakpoints.down('md')]: {
+  //   backgroundColor: 'red',
+  // },
+}));
+
+const Heading = styled(Typography)(({ theme }) => ({
+  color: theme.palette.secondary.dark,
+  fontSize: '48px',
+  fontWeight: 700,
+  textAlign: 'center',
+}));
+
+const Container = styled(Box)(() => ({
+  display: 'flex',
+  marginTop: '25px',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <Wrapper>
+      <Heading>Sign Up</Heading>
+      <Container>
+        <SignupForm />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Signup;
