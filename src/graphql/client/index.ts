7ac25b9f343551531aa5bc6dc3aaaffa75b23c7b
Abrar Azam <abrar.azam@kwanso.com>
2023-03-09 11:12:12 +0500

    send bearer token everytime request is made to backend

--- src/graphql/client/index.ts
+++ src/graphql/client/index.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem('auth_token');
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
 
 const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
   cache: new InMemoryCache(),
-  uri: 'http://localhost:3001/graphql',
 });
 
 export default client;
