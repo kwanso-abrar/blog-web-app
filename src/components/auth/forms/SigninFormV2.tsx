1065959372b32dbe1a6fbe946e4e6f5e5d6cb8f1
Abrar Azam <abrar.azam@kwanso.com>
2023-03-14 17:22:11 +0500

    design signin page

--- src/components/auth/forms/SigninFormV2.tsx
+++ src/components/auth/forms/SigninFormV2.tsx
@@ -1,7 +1,19 @@
+import { Link, useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Box, Button, IconButton, InputAdornment, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Divider,
+  Checkbox,
+  FormGroup,
+  TextField,
+  IconButton,
+  Typography,
+  InputAdornment,
+  FormControlLabel
+} from '@mui/material';
 
 const Wrapper = styled(Box)(() => ({
   marginTop: '74px'
@@ -20,8 +32,19 @@ const VisibilityIconText = styled(Typography)(() => ({
   color: '#666666'
 }));
 
-export const SignupFormV2 = () => {
-  const [showPassword, setShowPassword] = useState(false);
+const ForgetPassword = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  fontSize: '16px',
+  fontWeight: 400,
+  lineHeight: '24px',
+  display: 'block',
+  textAlign: 'right',
+  marginTop: '5px'
+}));
+
+export const SigninFormV2 = () => {
+  const [showPassword, setShowPassword] = useState(true);
+  const navigate = useNavigate();
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
@@ -30,26 +53,17 @@ export const SignupFormV2 = () => {
         <form>
           <Box sx={{ width: '712px' }}>
             <InputField
-              placeholder="Enter your full name"
               variant="outlined"
-              label="What’s your full name?"
+              label="Email address or user name"
               InputLabelProps={{ shrink: true, sx: { top: '-20px', fontSize: '18px', lineHeight: '150%' } }}
             />
           </Box>
+
           <Box sx={{ marginTop: '50px', width: '712px' }}>
             <InputField
-              placeholder="Enter your email address"
-              variant="outlined"
-              label="What’s your email?"
-              InputLabelProps={{ shrink: true, sx: { top: '-20px', fontSize: '18px', lineHeight: '150%' } }}
-            />
-          </Box>
-          <Box sx={{ marginTop: '50px', width: '712px' }}>
-            <InputField
-              placeholder="Enter your password"
               variant="outlined"
               type={showPassword ? 'text' : 'password'}
-              label="Create a password"
+              label="Password"
               InputLabelProps={{ shrink: true, sx: { top: '-20px', fontSize: '18px', lineHeight: '150%' } }}
               InputProps={{
                 endAdornment: (
@@ -70,16 +84,41 @@ export const SignupFormV2 = () => {
                   </InputAdornment>
                 )
               }}
-              helperText={'Use 8 or more characters with a mix of letters, numbers & symbols'}
             />
+            <ForgetPassword to="#">Forget your password</ForgetPassword>
           </Box>
+
+          <FormGroup>
+            <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
+          </FormGroup>
 
           <Box sx={{ marginTop: '40px' }}>
             <Button variant="auth" disabled>
-              Create an account
+              Log in
             </Button>
           </Box>
         </form>
+      </Box>
+      <Divider sx={{ marginTop: '40px', borderBottom: '2px solid #E5E5E5' }} />
+      <Box sx={{ marginTop: '40px' }}>
+        <Typography sx={{ color: '#333333', fontSize: '27px', lineHeight: '140%', textAlign: 'center' }}>
+          Don’t have an account?
+        </Typography>
+        <Button
+          variant="outlined"
+          sx={{
+            width: '100%',
+            marginTop: '20px',
+            height: '64px',
+            borderRadius: '40px',
+            fontSize: '21px',
+            fontWeight: '600',
+            lineHeight: '140%'
+          }}
+          onClick={() => navigate('/signup')}
+        >
+          Sign up
+        </Button>
       </Box>
     </Wrapper>
   );
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,37 +1,30 @@
 import { styled } from '@mui/material/styles';
-import { SignInForm } from 'components';
 import { Box, Typography } from '@mui/material';
-
-const Wrapper = styled(Box)(({ theme }) => ({
-  paddingTop: '88px',
-  backgroundColor: theme.palette.secondary.contrastText,
-
-  // [theme.breakpoints.down('md')]: {
-  //   backgroundColor: 'red',
-  // },
-}));
+import { SigninFormV2 } from 'components/auth/forms/SigninFormV2';
 
 const Heading = styled(Typography)(({ theme }) => ({
-  color: theme.palette.secondary.dark,
-  fontSize: '48px',
-  fontWeight: 700,
-  textAlign: 'center',
+  color: theme.palette.primary.main,
+  fontSize: '27px',
+  fontWeight: 600,
+  lineHeight: '140%',
+  textAlign: 'center'
 }));
 
 const Container = styled(Box)(() => ({
   display: 'flex',
   marginTop: '25px',
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'center'
 }));
 
 export const Signin = () => {
   return (
-    <Wrapper>
-      <Heading>Sign in</Heading>
+    <Box>
+      <Heading>Log in</Heading>
       <Container>
-        <SignInForm />
+        {/* <SignInForm /> */}
+        <SigninFormV2 />
       </Container>
-    </Wrapper>
+    </Box>
   );
 };
