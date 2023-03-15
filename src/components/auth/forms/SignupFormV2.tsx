0eaf34d38947789f6fa08a3d81dcb1acd15cb1f2
Abrar Azam <abrar.azam@kwanso.com>
2023-03-15 12:46:50 +0500

    hook signup graphql api

--- src/components/auth/forms/SignupFormV2.tsx
+++ src/components/auth/forms/SignupFormV2.tsx
@@ -1,64 +1,160 @@
+import toast from 'react-hot-toast';
 import { useState } from 'react';
-import { InputField, VisibilityIconText } from 'styles';
+import { saveToken } from 'utils';
+import { yupSchema } from 'formValidations';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useContextApi } from 'AppContext';
+import { Controller, useForm } from 'react-hook-form';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Box, Button, IconButton, InputAdornment } from '@mui/material';
+import { InputField, VisibilityIconText } from 'styles';
+import { useSignInMutation, useSignUpMutation } from 'generated';
+import { Backdrop, Box, Button, CircularProgress, IconButton, InputAdornment } from '@mui/material';
+
+const schema = yupSchema.signUp;
 
 export const SignupFormV2 = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useContextApi();
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: { dirtyFields }
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  const [signUp, { loading: signupLoading }] = useSignUpMutation({
+    onCompleted: () => {
+      toast.success('Successfully Signed up');
+    },
+    onError: () => {
+      toast.error('User already registerd');
+    }
+  });
+
+  const [signIn, { loading: signInLoading }] = useSignInMutation({
+    onCompleted: (data) => {
+      if (data.signIn.accesstoken) {
+        saveToken(data.signIn.accesstoken);
+        setIsLoggedIn(true);
+        navigate('/');
+      }
+    },
+    onError: () => {
+      toast.error('Invalid Credentials!');
+    }
+  });
+
+  const onFormSubmit = (values: any) => {
+    signUp({
+      variables: {
+        name: values.name,
+        email: values.email,
+        password: values.password
+      }
+    }).then((response) => {
+      if (!response.errors) {
+        signIn({
+          variables: {
+            email: values.email,
+            password: values.password
+          }
+        });
+      }
+    });
+  };
 
   return (
     <Box sx={{ marginTop: '74px' }}>
+      {(signupLoading || signInLoading) && (
+        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={signupLoading || signInLoading}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
       <Box>
-        <form>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
           <Box sx={{ width: '712px' }}>
-            <InputField
-              placeholder="Enter your full name"
-              variant="outlined"
-              label="What’s your full name?"
-              InputLabelProps={{ shrink: true }}
+            <Controller
+              control={control}
+              name="name"
+              render={({ field, fieldState: { error } }) => (
+                <InputField
+                  {...field}
+                  error={!!error}
+                  helperText={error?.message}
+                  label="What’s your full name?"
+                  variant="outlined"
+                  placeholder="Enter your full name"
+                  InputLabelProps={{ shrink: true }}
+                />
+              )}
             />
           </Box>
           <Box sx={{ marginTop: '50px', width: '712px' }}>
-            <InputField
-              placeholder="Enter your email address"
-              variant="outlined"
-              label="What’s your email?"
-              InputLabelProps={{ shrink: true }}
+            <Controller
+              control={control}
+              name="email"
+              render={({ field, fieldState: { error } }) => (
+                <InputField
+                  {...field}
+                  error={!!error}
+                  helperText={error?.message}
+                  placeholder="Enter your email address"
+                  variant="outlined"
+                  label="What’s your email?"
+                  InputLabelProps={{ shrink: true }}
+                />
+              )}
             />
           </Box>
           <Box sx={{ marginTop: '50px', width: '712px' }}>
-            <InputField
-              placeholder="Enter your password"
-              variant="outlined"
-              type={showPassword ? 'text' : 'password'}
-              label="Create a password"
-              InputLabelProps={{ shrink: true }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end" sx={{ position: 'absolute', right: '0', top: '-20px' }}>
-                    <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword}>
-                      {showPassword ? (
-                        <>
-                          <VisibilityOff sx={{ width: '24px', height: '24px' }} />
-                          <VisibilityIconText>Hide</VisibilityIconText>
-                        </>
-                      ) : (
-                        <>
-                          <Visibility sx={{ width: '24px', height: '24px' }} />
-                          <VisibilityIconText> Show</VisibilityIconText>
-                        </>
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-              helperText={'Use 8 or more characters with a mix of letters, numbers & symbols'}
+            <Controller
+              control={control}
+              name="password"
+              render={({ field, fieldState: { error } }) => (
+                <InputField
+                  {...field}
+                  error={!!error}
+                  helperText={error ? error?.message : 'Use 8 or more characters with a mix of letters, numbers & symbols'}
+                  placeholder="Enter your password"
+                  variant="outlined"
+                  type={showPassword ? 'text' : 'password'}
+                  label="Create a password"
+                  InputLabelProps={{ shrink: true }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end" sx={{ position: 'absolute', right: '0', top: '-20px' }}>
+                        <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword}>
+                          {showPassword ? (
+                            <>
+                              <VisibilityOff sx={{ width: '24px', height: '24px' }} />
+                              <VisibilityIconText>Hide</VisibilityIconText>
+                            </>
+                          ) : (
+                            <>
+                              <Visibility sx={{ width: '24px', height: '24px' }} />
+                              <VisibilityIconText> Show</VisibilityIconText>
+                            </>
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+              )}
             />
           </Box>
 
           <Box sx={{ marginTop: '40px' }}>
-            <Button variant="auth" disabled>
+            <Button variant="auth" disabled={!(dirtyFields.name && dirtyFields.email && dirtyFields.password)} type="submit">
               Create an account
             </Button>
           </Box>
