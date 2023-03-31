a8166e08bb4c8989f16a216c23a53cdd6d01a434
Abrar Azam <abrar.azam@kwanso.com>
2023-03-31 12:09:29 +0500

    use env variable for graphql api server

--- src/graphql/client/index.ts
+++ src/graphql/client/index.ts
@@ -2,7 +2,7 @@ import { ROUTES_PATH } from '../../constants';
 import { getToken, removeToken } from 'utils';
 import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' });
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL });
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = getToken();
