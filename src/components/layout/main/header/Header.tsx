249d80601fcbf9476d7723acede5fff4aba5b823
Abrar Azam <abrar.azam@kwanso.com>
2023-04-06 12:52:43 +0500

    minor fixe

--- src/components/layout/main/header/Header.tsx
+++ src/components/layout/main/header/Header.tsx
@@ -15,10 +15,7 @@ export const Header = () => {
 
   const onHeaderScroll = useCallback(() => {
     if (headerRef.current) {
-      if (
-        window.scrollY - (headerRef.current?.offsetHeight + 250) >
-        headerRef.current?.offsetHeight
-      ) {
+      if (window.scrollY > headerRef.current?.offsetHeight) {
         headerRef.current.style.position = 'fixed';
         headerRef.current.style.top = '0px';
         headerRef.current.style.boxShadow = '0 3px 5px rgba(57, 63, 72, 0.3)';
