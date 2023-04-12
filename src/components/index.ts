3d3181d2484cace5ddc7e754c71e0f17a59c0788
Abrar Azam <abrar.azam@kwanso.com>
2023-04-12 09:24:05 +0500

    fix naming conventions

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,5 @@ export * from './auth';
 export * from './common';
 export * from './layout';
 export * from './ReadBlog';
+export * from './messenger';
 export * from './createPost';
-export * from './chatPlayground';
